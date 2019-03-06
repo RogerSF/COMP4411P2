6314549f552f3939690df1674d6e5a3bb534c488
RogerSF <rushao@umich.edu>
2019-03-06 22:30:12 +0800

    Add: Carousel setup

--- sample.cpp
+++ sample.cpp
@@ -4,7 +4,7 @@
 #include "modelerapp.h"
 #include "modelerdraw.h"
 #include <FL/gl.h>
-
+#include <math.h>
 #include "modelerglobals.h"
 
 // To make a SampleModel, we inherit off of ModelerView
@@ -46,55 +46,55 @@ void SampleModel::draw()
 	setDiffuseColor(COLOR_GREEN);
 	glPushMatrix();
 	glTranslated(VAL(XPOS), VAL(YPOS), VAL(ZPOS));
-
+		// draw base
 		glPushMatrix();
-		glTranslated(-1.5, 0, -2);
-		glScaled(3, 1, 4);
+		glTranslated(-1.5, 0, -1.5);
+		glScaled(3, 0.3, 3);
 		drawBox(1,1,1);
 		glPopMatrix();
 
-		// draw arms
+		// draw central column
 		glPushMatrix();
 		glRotated(VAL(ROTATE), 0.0, 1.0, 0.0);
 		glRotated(-90, 1.0, 0.0, 0.0);
-		drawCylinder(4+VAL(HEIGHT), 0.1, 0.1);
+		drawCylinder(5+VAL(HEIGHT), 0.1, 0.1);
 
-		glTranslated(0.0, 0.0, 4+VAL(HEIGHT));
+		glTranslated(0.0, 0.0, 5+VAL(HEIGHT));
 		
-		drawCylinder(1, 5, 0.5);
+		drawCylinder(1, 5, 0.1);
 		glTranslated(0,0,2);
 		drawSphere(1);
 
-		glTranslated(0.0, 3.5, -5);
-		drawCylinder(3, 0.1, 0.2);
+		glTranslated(0.0, 3.5, -5-sin(VAL(ROTATE)*M_PI/180));
+		drawCylinder(3+sin(VAL(ROTATE)*M_PI/180), 0.1, 0.2); //sin(VAL(ROTATE)*M_PI/180);
 		drawSphere(0.5);
 
-		glTranslated(3.5, -3.5, 0);
-		drawCylinder(3, 0.1, 0.2);
+		glTranslated(3.5, -3.5, sin(VAL(ROTATE)*M_PI/180) - sin((VAL(ROTATE)+90)*M_PI/180));
+		drawCylinder(3+sin((VAL(ROTATE)+90)*M_PI/180), 0.1, 0.2);
 		drawSphere(0.5);
 
-		glTranslated(-7, 0, 0);
-		drawCylinder(3, 0.1, 0.2);
+		glTranslated(-7, 0, sin((VAL(ROTATE)+90)*M_PI/180) - sin((VAL(ROTATE)-90)*M_PI/180));
+		drawCylinder(3+sin((VAL(ROTATE)-90)*M_PI/180), 0.1, 0.2);
 		drawSphere(0.5);
 
-		glTranslated(3.5, -3.5, 0);
-		drawCylinder(3, 0.1, 0.2);
+		glTranslated(3.5, -3.5, sin((VAL(ROTATE)-90)*M_PI/180) - sin((VAL(ROTATE)+180)*M_PI/180));
+		drawCylinder(3+sin((VAL(ROTATE)+180)*M_PI/180), 0.1, 0.2);
 		drawSphere(0.5);
 
-		glTranslated(2.47487, 1.02513, 0);
-		drawCylinder(3, 0.1, 0.2);
+		glTranslated(2.47487, 1.02513, sin((VAL(ROTATE)+180)*M_PI/180) - sin((VAL(ROTATE)+135)*M_PI/180));
+		drawCylinder(3+sin((VAL(ROTATE)+135)*M_PI/180), 0.1, 0.2);
 		drawSphere(0.5);
 
-		glTranslated(-4.94974, 0, 0);
-		drawCylinder(3, 0.1, 0.2);
+		glTranslated(-4.94974, 0, sin((VAL(ROTATE)+135)*M_PI/180) - sin((VAL(ROTATE)-135)*M_PI/180));
+		drawCylinder(3+sin((VAL(ROTATE)-135)*M_PI/180), 0.1, 0.2);
 		drawSphere(0.5);
 
-		glTranslated(0, 4.94974, 0);
-		drawCylinder(3, 0.1, 0.2);
+		glTranslated(0, 4.94974, sin((VAL(ROTATE)-135)*M_PI/180) - sin((VAL(ROTATE)-45)*M_PI/180));
+		drawCylinder(3+sin((VAL(ROTATE)-45)*M_PI/180), 0.1, 0.2);
 		drawSphere(0.5);
 
-		glTranslated(4.94974, 0, 0);
-		drawCylinder(3, 0.1, 0.2);
+		glTranslated(4.94974, 0, sin((VAL(ROTATE)-45)*M_PI/180) - sin((VAL(ROTATE)+45)*M_PI/180));
+		drawCylinder(3+sin((VAL(ROTATE)+45)*M_PI/180), 0.1, 0.2);
 		drawSphere(0.5);
 
 
@@ -113,7 +113,8 @@ int main()
     controls[YPOS] = ModelerControl("Y Position", 0, 5, 0.1f, 0);
     controls[ZPOS] = ModelerControl("Z Position", -5, 5, 0.1f, 0);
     controls[HEIGHT] = ModelerControl("Height", 1, 2.5, 0.1f, 1);
-	controls[ROTATE] = ModelerControl("Rotate", -135, 135, 1, 0);
+	controls[ROTATE] = ModelerControl("Rotate", -180, 180, 1, 0);
+	controls[SEAT_ROTATE] = ModelerControl("Seat Rotate", -180, 180, 1, 0);
 
     ModelerApplication::Instance()->Init(&createSampleModel, controls, NUMCONTROLS);
     return ModelerApplication::Instance()->Run();
